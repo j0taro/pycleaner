62b18ed428983d693ba98503fd91a3bbde1fed5f
jotaro <93826447+j0taro@users.noreply.github.com>
2022-09-10 16:37:31 +0300

    Update setup-requirements.bat

--- setup-requirements.bat
+++ setup-requirements.bat
@@ -1,17 +1,5 @@
 
 @echo off
-python --version 2>NUL
-if errorlevel 1 goto errorNoPython
-if errorlevel 0 goto ong
-
-
-:errorNoPython
-echo.
-echo Error^: Python not installed
-curl -L "https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe" -O
-start /W python-3.10.7-amd64.exe
-:ong
-echo python installed
 cd %~dp0
 MD tools
 pip install -r requirements.txt
